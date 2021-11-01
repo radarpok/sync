50aabe886765b136f910a800132d66cbfe8ee901
radarpok <vdiana.m8@gmail.com>
2021-11-01 03:07:05 +0100

    adding the common set of constellation types

---
+++ konstellaciok/kozos_jellemzoik.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-multi-str */
+module.exports.biologiai_kulonprogramok=()=>{
+
+let konstellaciok = [{
+
+    kozos_jellemzoik: {
+        1: "konstelláció fogalma: egyidejűleg több Hameri-góc található a fejben",
+        2: "halmozott érzelmi megrázkódtatás hatására a lélek-agy-szerv párhuzamosan és szinkronban lezajló elváltozást hoz létre az agyban",
+        3: "elmezavar csak akkor lép fel, amikor valakinek ugyanazon agyi részben jobb és a bal oldalon egyidejűleg van egy-egy biológiai különprogramja szimpatikotóniás együttállás esetén",
+        4: "szimpatikotóniás együttállás: két konfliktusaktív szakasz esik egybe / egy konfliktusaktív szakasz esik egybe egy epileptokrízissel / két epileptokrízis esik egybe",
+        5: "nem minden típusú konstelláció jár specifikus elmezavarral, ezen esetekben már 'csak' a gyógyulási tünetek vehetőek észre",
+        6: "ha nincs észrevéve és azonosítva a jelenség, mások is könnyen 'bekonstellálódhatnak' egy konstellált személy hatására",
+        
+    az_ismert_konstellaciok_csoportositasa: {
+        A: "agytörzsi",
+        B: "kisagyi",
+        C: "velőállományi (legdestruktívabb tünetekkel járó elmezavart okozza)",
+        D: "kéregállományi" [{
+                nemi_birtok_zona: {
+                    a: "gége és hörgő asztma konstelláció kombinációk",
+                    b: "lebegő-konstelláció",
+                    c: "autista-konstelláció",
+                    d: "mytho-konstelláció",
+                    e: "agresszív-konstelláció",
+                    f: "anorexia-konstelláció",
+                    g: "posztmortális-konstelláció",
+                    h: "Casanova- és nympho-konstelláció",
+                    i: "bulémia-konstelláció",
+                    j: "hallókéreg-nemi/birtok konstelláció"
+                },
+                premotorikus_szenzorikus_es_motorikus_keregmezo,
+                szenzorikus_es_posztszenzorikus_keregmezo,
+                latokeregmezo_es_hallokeregmezo,
+                preomotorikus_szenzorikus_es_a_latokereg_konstellacioi: {
+                    k: "frontál-para-konstelláció",
+                    l: "beszorítottság-para-konstelláció",
+                    m: "üldözési-para-konstelláció"
+                }
+            }]
+        }
+    },
+}]
+return console.log(konstellaciok)
+};
