ba894f1f91eae1fc593f54882673c2e412dec6dc
radarpok <vdiana.m8@gmail.com>
2021-11-02 22:20:48 +0100

    adding more available data to special biological programs

---
+++ biologiai_kulonprogramok/biologiai_kulonprogram.js
@@ -0,0 +1,21 @@
+module.exports.biologika=()=>{
+let ertelmes_biologiai_kulonprogram = [{
+        
+    1: "Amit korábban betegségnek véltünk, az nem más, mint egy előre megjósolható és kiszámítható értelmes biológiai válaszreakció, egy szervi elváltozás\
+        neve, címkéje, tünete, fizikai vagy mentális jelensége. Egy semleges tünet, se nem jó, se nem rossz. Megmutatja, mi zajlik, vagy mi zajlott a lelkivilágunkban.\
+        Ez azt jelenti, hogy az összes elváltozás, ami nem mérgezés, baleset, sugárzás és kifejlődési rendellenesség következtében alakul ki, a lélek\
+        kihatására jön létre. Az elváltozások biológiai célja, értelme: alkalmazkodás a konfliktushelyzethez.",
+    2: "Az Új Orvostudomány tudományosan igazolt megállapításai szerint: a megbetegedés az Anyatermészet egy Értelmes Biológiai Különprogramja, mely két\
+        betegségi szakaszból áll. A megbetegedés a Lélekben - Agyban - Szerven párhuzamosan és szinkronban zajló elváltozás, melyet a\
+        szervezetünk konkrét feltételeknek megfelelő érzelmi megrázkódtatásra kapcsol be, egy ÉRTELMES, ésszerű, biológiai, élettani, túlélési válaszreakció.",
+    3: "Ezekre a Különprogramokra normál (kiegyensúlyozott) állapotban nincs szükség, ezért nem is tapasztaljuk őket. Viszont, ha\
+        teljesülnek konkrét feltételek, az érzelmi konfliktus biológiai elősegítése érdekében beindul egy szervi elváltozás, daganat vagy sorvadás\
+        keletkezik a megbetegedés első, ún. konfliktus-aktív szakaszában. A konfliktus megoldása után a létrejött elváltozás - normál esetben -\
+        helyreállításra kerül, ez a második, daganatelbontási, sorvadás-újjáépítési szakasz.\
+        A hosszan tartó, visszaeső vagy visszatérő megbetegedéseknél nem olyan egyszerű a helyzet, már ami a konfliktusfeltárást és a számolásokat illeti -\
+        ehhez kell feltárni az ún. 'síneket' is.",
+    4: "Online elérhető adatok az összes ismert Különprogramról - Biologika Szerv Atlasz feltöltött lapjai:\
+        https://szervatlasz.hu/megtekintes/a-szerv-atlasz-lapok-megtekintese-egyenkent.html"
+}]
+return console.log(ertelmes_biologiai_kulonprogram)
+};
