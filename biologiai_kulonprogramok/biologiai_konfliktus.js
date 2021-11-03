88a31701e61ba06a144827dda29c76d72a2308ca
radarpok <vdiana.m8@gmail.com>
2021-11-03 22:51:47 +0100

    adding more available data to special biological conflicts

---
+++ biologiai_kulonprogramok/biologiai_konfliktus.js
@@ -0,0 +1,27 @@
+module.exports.biologika=()=>{
+let biologiai_konfliktus = [{
+        
+    1: "Az életfunkciók és vezérlés egyensúlyában akkor történik tartós kibillenés, akkor indul be elváltozás, ha bekövetkezik az ún. biológiai konfliktus\
+        (DHS - Dirk Hamer Szindróma). A DHS legalapvetőbb módon abban különbözik az összes többi konfliktustól, hogy testileg is érzékelhető, felismerhető\
+        alapváltozást idéz elő (pl. hideg végtagok, étvágytalanság, lelki szinten kényszeres gondolkozás a konfliktus témájában...stb). Hatására a\
+        lélekben, az agyban és a szerven szinkronban zajlik le szubjektív, egyéni, érzelmileg megélt, de létező, kimutatható érzelmi megütközés,\
+        megrökönyödés, leblokkolás vagy éppen kibillenés, heveskedés, drámázás. Valódi lehetőségünk van választani (vezérelni elváltozásainkat), és ha\
+        figyelmetlenek vagy hanyagok vagyunk, kárt tehetünk önmagunkban.\
+        DHS csak akkor következik be, ha mind az öt alábbi feltétel egyszerre áll fenn:",
+
+        A:  "A konfliktus témája, tárgya egy komolynak érzett, lesújtó, lehengerlő, nyugtalanító, nyomasztó, elkeserítő, kiábrándító, kétségbe ejtő,\
+            idegesítő, felzaklató, félelmet keltő dolog, esemény, szituáció, melyben érintettnek, hibásnak, felelősnek, áldozatnak vagy egyedül illetékesnek\
+            érezzük magunkat. A dolog lehet enyhe bosszúság, de lehet egy dühös, heves érzelmi kitörés, vagy egy brutális, drámai sokkélmény is. A lényeg:\
+            tartósan kibillent a lelki nyugalmunkból és a figyelmünk huzamosabb ideig feszülten egy dologra koncentrálódik (kényszeres gondolkozás jelenik meg).",
+        
+        B:  "Váratlanul, felkészületlenül ér, a meglepetés erejével hat. A dolog, esemény, szituáció akkor következik be, amikor nem számítunk rá.",
+        
+        C:  "A konfliktus-helyzetet elfogadhatatlannak éljük meg. Megütközés, válság, életviteli akadály, a konfliktus tárgya akadályoz a nyugodt létezésben,\
+            normál életvitelben.",
+        
+        D:  "Elszigetelten éljük meg (azonosulunk az állapotunkkal, szituációval, magunkba fojtjuk, nem beszélünk róla, érzelmeinket nem osztjuk meg.",
+        
+        E:  "Átmenetileg nincs megoldása, eltart egy darabig. (A konfliktus időtartamából és intenzitásából adódik az ún. konfliktustömeg.)"
+}]
+return console.log(biologiai_konfliktus)
+};
