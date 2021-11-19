6a4f9b959cda00528fa1fb9c5019e2493e1dbcf2
dio <vdiana.m8@gmail.com>
2021-11-20 00:25:00 +0100

    adding more data to constellations

---
+++ konstellaciok/elmezavarok_tipusai/!_veloallomanyi_konstellacio.js
@@ -0,0 +1,53 @@
+module.exports.konstellaciok=()=>{
+
+let veloallomanyi_konstellacio = [{
+
+    ok: {
+        1:"Csalódás, elkeseredés, kudarc, sikertelenségi, vesztességi érzést, ún. önbecsülés-letörést él meg valaki anya-gyermek és partneri vonatkozásban\
+        EGYARÁNT. Ennek a hatására jelennek meg a velőállománynak a jobb és bal oldalán is egyszerre léziós gócok ('kimegy a biztosíték'), ennek a\
+        hatására változik meg az érintettnek jelentősen a viselkedése, értékítélete.",
+
+        2:"Az öntúlértékeléssel és értékelészavarral fékezi agyunk a konfliktusok intenzitását, ezáltal a sorvadások súlyosbodását. Mivel a formális-logikai,\
+        intellektuális gondolkodás nem sérül (mint a skizofréniánál), ez az elmezavar különösen veszélyes, az érintett kicselezi, csalfa, hazug módon\
+        letagadja a cselekedeteit, viselkedését, és azt, hogy árt másoknak. 'Még magának is hazudik.' - csak nem veszi magát észre. A tettei attól még\
+        bizonyíthatóak.",
+        
+        3:"A legtöbb ember éppen ez idő alatt, az értékítélő-képességének megborulása miatt teszi tönkre magát kapcsolatilag és anyagilag, nem azért,\
+        mert ostoba vagy hanyag volna. A konstellált megfigyelhető módon éppen az önértékelés-letörési vonatkozásában fölényeskedik, nagyzási, bizonyítási\
+        kényszere van. Bizonyítani akar a megalázónak, elnyomónak (bárkinek, akihez képest alulmaradottnak érzi magát), ezért kényszeresen lekezel,\
+        lealáz, megaláz, elnyom másokat, élősködik rajtuk. 'Ez nem az ördög műve, és nem rosszindulat, ez egy primitív mentális biológiai túlélő-szoftver.'\
+        /B.R./"
+        },
+
+    eszlelheto: [{
+
+        1: "a legveszélyesebb az összes elmezavar közül (hosszú ideig fennálhat, és a tudatlan ember számára nem vagy nehezen észrevehető)",
+        2: "ambivalencia ('jót akarok neked, de mégis ártok')",
+        3: "értékítéleti zavar (önzés; egoizmus; gátlástalanság; vakmerőség; önmagát kényszeresen fel-, másokat kényszeresen lebecsül)",
+        4: "tisztességtelenség, lekezelés, 'kivagyiság', dominancia (ráerőlteti az akaratát másokra), provokációs- és uszítás kényszer",
+        5: "megalomán viselkedés (nagyzási hóbort)",
+        6: "cezaromán viselkedés (uralkodási kényszer; meg akarja mondani, akkor is, ha jelentéktelen, hogy a másik mit csináljon)",
+        7: "nárcisztikus viselkedés (megtévesztő önfényezés, öndicséret, 'mindenkinek olyannak kéne lennie, mint én')",
+        8: "fasiszta viselkedés (kényszeres megkülönböztetés: 'mindenki akin kockás ing van, az jó ember, mindenki akin nincs, az ne éljen')",
+        9: "átverési kényszer, árulás, lódítás",
+        10: "bizonyítási, produkálási kényszer",
+        11: "kényszeres megalázás és behódolás, gyalázás, gyötrés, ellenőrzés, játszmázás, élősködés ('Mert én megérdemlem, hogy a barmok eltartsanak')",
+        12: "ellenségeskedés, leküzdési kényszer ('én úgy vagyok tökéletes, ahogy vagyok, bezzeg a másik')",
+        13: "nem látja át, hogy a tetteivel mit okoz, hogy mi lesz a következménye",
+        14: "szélsőséges viselkedés, mentális erőszak ('féljetek tőlem!')",
+        15: "minimum 1-2 hónapos megfigyelés szükséges hozzá"
+    }],
+
+    megoldas: {
+
+        1:"A barbarizmus, kannibalizmus, ártás, mások bántása, az élősködés, a kapzsiság, a mértéktelenség stb., az önbecsülési konfliktusok megoldásával,\
+        az önértékelés helyreáll(ít)ásával (a különprogramok kifutásával) elmúlik."
+        },
+
+    lehetseges_visszaeses: {
+
+        1:"Ha a konfliktusok nem kerülnek feloldásra, a szervi diszfunkció 'idő előtt' végezhet az egyénnel/egyeddel."
+        },
+}]
+return console.log(veloallomanyi_konstellacio)
+};
